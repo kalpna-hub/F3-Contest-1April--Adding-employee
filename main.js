f9d156a2be105209531045c78e412271cb9da926
kalpna-hub <122824448+kalpna-hub@users.noreply.github.com>
2023-04-22 12:25:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,16 @@
-var count = 0;
-var employees = [];
-const btn = document.getElementById("add-employee-button");
-const message = document.getElementById("message");
+var nameInput = document.getElementById("name");
+var professionInput = document.getElementById("profession");
+var ageInput = document.getElementById("age");
 
-function addEmployees(e) {
-    e.preventDefault();
-
-    const nameValue = document.getElementById('name').value;
-    const professionValue = document.getElementById('profession').value;
-    const ageValue = document.getElementById('age').value;
-    
-
-    //Inputs Mandatory, Blank will not be submitted.
-    if (nameValue == "" || professionValue == "" || ageValue == "") {
-        message.innerText = `Error : Please Make sure All the fields are filled before adding in an employee !`;
-        message.style.color = "red";
-        message.style.fontSize = "15px";
-        message.style.marginBottom = "15px";
-        return;
-      }
-
-      let obj =
+nameInput.addEventListener("change", function (event) {
+  console.log("event", event);
+});
+let obj =
       {
         id : count++,
-        name : nameValue,
-        profession: professionValue,
-        age : ageValue,
+        name : name,
+        profession: profession,
+        age : age,
       }
       employees.push(obj);
 
@@ -34,28 +19,34 @@ function addEmployees(e) {
       message.style.fontSize = "20px";
       message.style.marginBottom = "15px";
 
-      
+var button = document.getElementById("adduser");
 
-    //  Clear the input fields   
-    document.getElementById('name').value = "";
-    document.getElementById('profession').value = "";
-    document.getElementById('age').value = "";
+button.addEventListener("click", (event) => {
+  console.log(event);
+    var name = nameInput.value;
+    var profession = professionInput.value;
+    var age = ageInput.value;
 
+    if (name == "" || profession == "" || age == "") {
+      document.querySelector(".status-msg").style.display = "block";
+      document.querySelector(".status-msg").style.color = "red";
+    } else {
+      document.querySelector(".emp-li-container").innerHTML += `
+          <div class="emp-card" id="id-${name}-${profession}-${age}">
+              <div class="details">
+                  <span>Name: ${name}</span>
+                  <span>Profession: ${profession}</span>
+                  <span>Age: ${age}</span>
+              </div>
+              <button id="del-user-btn" onclick="deleteUser('id-${name}-${profession}-${age}')">Delete User</button>
+          </div>
+      `;
+    }
+});
 
-    // create a function to add list and call here thats it
-   // let employees = [];
-    function addEmployee(name, profession, age) {
-        const employee = {
-          id: employees.length + 1,
-          name: nameValue,
-          profession: professionValue,
-          age: ageValue
-        };
-      
-        employees.push(employee);
-      }
-      console.log(employees);
+function deleteUser(id) {
+  var userToDel = document.getElementById(id);
+  console.log(userToDel);
+  document.querySelector(".emp-li-container").removeChild(userToDel);
+  alert("User deleted");
 }
-
-
-btn.addEventListener('click',addEmployees )
